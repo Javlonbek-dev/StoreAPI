f2f7cb119c46e0da38eacc9bd1c6fb8d61a06a49
javlonrahmatullayev07 <javlonrahmatullayev07@gmail.com>
2023-07-30 21:51:05 +0300

    CINTROLLER:  GET  GetProductCategory

---
+++ Solution1/StoryAPI/Controllers/ShoppingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoryAPI.DataAccess;
+
+namespace StoryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+        readonly IDataAccess dataAccess;
+        private readonly string DateFormat;
+
+        public ShoppingController(IDataAccess dataAccess, IConfiguration configuration)
+        {
+            this.dataAccess = dataAccess;
+            DateFormat = configuration["Constants:DateFormat"];
+        }
+
+        [HttpGet("GetCategoryList")]
+        public IActionResult GetCategoryList()
+        {
+            var result = dataAccess.GetProductCategories();
+
+            return Ok(result);
+        }
+    }
+}
